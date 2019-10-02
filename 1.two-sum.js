718fcc1df5c3745e07200af89fd413908c6a0b27
Amybiubiu <1246457100@qq.com>
2019-10-02 22:59:44 +0800

    initial

---
+++ 1.two-sum.js
@@ -0,0 +1,23 @@
+/*
+ * @lc app=leetcode id=1 lang=javascript
+ *
+ * [1] Two Sum
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    const comp = {};
+    for(let i=0; i<nums.length; i++){
+        if(comp[nums[i] ]>=0){
+            return [ comp[nums[i] ] , i]
+        }
+        comp[target-nums[i]] = i
+    }
+};
+// @lc code=end
+
