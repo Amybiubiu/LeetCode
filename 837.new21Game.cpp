aea2b8f195140c7828cc0109c1ad6a46bcfd6afe
Amybiubiu <1246457100@qq.com>
2021-02-09 00:19:16 +0800

    dp && math

---
+++ 837.new21Game.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    double new21Game(int N, int K, int W) {
+        if (K == 0) {
+            return 1.0;
+        }
+        vector<double> dp(K + W);
+        double s = 0;
+        for (int i = K; i < K + W; i++) {
+            dp[i] = i <= N ? 1.0 : 0.0;
+            s += dp[i];
+        }
+        for (int i = K - 1; i >= 0; i--) {
+            dp[i] = s/W;
+            s = s - dp[i+W] + dp[i];
+        }
+        return dp[0];
+    }
+};
