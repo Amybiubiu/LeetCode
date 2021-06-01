7caf57eac2aa21f2ef3f9df5d022cec775d3a572
Amybiubiu <1246457100@qq.com>
2021-06-01 19:46:46 +0800

    simulate

---
+++ 142.detectCycle.cpp
@@ -0,0 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *fast = head;
+        ListNode *slow = head;
+        while(fast){
+            if(fast->next){
+                fast = fast->next->next;
+            }else{
+                return nullptr;
+            }
+            slow = slow->next;
+            if(fast == slow){
+                fast = head;
+                while(fast != slow){
+                    fast = fast->next;
+                    slow = slow->next;
+                }
+                return fast;
+            }
+        }
+        return nullptr;
+    }
+};
---
+++ 54.spiralOrder.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> spiralOrder(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+        int total = m*n, cnt = 0;
+        int top = 0, bottom = m-1, left = 0, right = n-1;
+        vector<int> res;
+        while(total){
+            // to right
+            for(int i = left; i <= right && total; i++){
+                res.push_back(matrix[top][i]);
+                total--;
+                cout<<"right"<<total<<endl;
+            }
+            top++;
+            // to down
+            for(int i = top; i <= bottom && total; i++){
+                res.push_back(matrix[i][right]);
+                total--;
+                // cout<<"down"<<total<<endl;
+            }
+            right--;
+            // to left
+            for(int i = right; i >= left && total; i--){
+                // 如果不加 total 的话，由于i 符合 left 和 right 的范围，total会变为-1；
+                res.push_back(matrix[bottom][i]);
+                total--;
+                // cout<<"left"<<total<<endl;
+            }
+            bottom--;
+            // to up
+            for(int i = bottom; i >= top && total; i--){
+                res.push_back(matrix[i][left]);
+                total--;
+                // cout<<"up"<<total<<endl;
+            }
+            left++;
+        }
+        return res;
+    }
+};
