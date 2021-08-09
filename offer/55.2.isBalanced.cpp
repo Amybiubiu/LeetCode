81f421189c7d940fba993bee50845ce624700150
Amybiubiu <1246457100@qq.com>
2021-08-09 10:31:05 +0800

    2021.08.09

---
+++ offer/55.2.isBalanced.cpp
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        if(!root) return true;
+        int left = getDepth(root->left);
+        int right = getDepth(root->right);
+        return isBalanced(root->left) && isBalanced(root->right) && abs(left-right) < 2;
+    }
+    int getDepth(TreeNode* root){
+        if(!root) return 0;
+        return max(getDepth(root->left), getDepth(root->right))+1;
+    }
+};
